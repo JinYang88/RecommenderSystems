2520886d331e75cb405f9c07db5d9676e39c0094
Jinyang88 <liujy725@gmail.com>
2019-11-27 14:00:31 +0800

    update

--- featureRec/data/Dataprocess/Kfold_split/config.py
+++ featureRec/data/Dataprocess/Kfold_split/config.py
@@ -1,4 +1,4 @@
-DATA_PATH = './Avazu/'
+DATA_PATH = './Criteo/'
 TRAIN_I = DATA_PATH + 'train_i.txt'
 TRAIN_X = DATA_PATH + 'train_x.txt'
 TRAIN_Y = DATA_PATH + 'train_y.txt'
--- featureRec/data/criteo_preprocess.sh
+++ featureRec/data/criteo_preprocess.sh
@@ -1,4 +1,2 @@
-mkdir Criteo
-python_slurm ./Dataprocess/Criteo/preprocess.py
 python_slurm ./Dataprocess/Kfold_split/stratifiedKfold.py
 python_slurm ./Dataprocess/Criteo/scale.py
